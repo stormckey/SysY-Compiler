0d7c681133f3a9c9ac35e48cda371218c991c324
Docker <docker@example.com>
2024-04-14 17:34:02 +0100

    more concise print tree

--- err.ml
+++ err.ml
@@ -7,10 +7,15 @@ let quit_type_mismatch_error ty1 ty2 =
 
 let id_not_found_error id = failwith (Printf.sprintf "id:%s is not defined" id)
 
-let check_type_mismatch ty1 ty2 tree1 tree2 =
+let check_type_mismatch tree2 ty1 ty2 tree1 =
   if ty1 != ty2 then (
     PrintBox_text.output stdout tree1;
     print_endline "\n -------------------in--------------------";
     PrintBox_text.output stdout tree2;
     print_endline "";
     quit_type_mismatch_error ty1 ty2)
+
+let fail_with_tree tree info =
+  PrintBox_text.output stdout tree;
+  print_newline ();
+  failwith info
--- typechecker.ml
+++ typechecker.ml
@@ -8,29 +8,31 @@ open Util
 
 let rec typecheck_lval ctxes (name, exp_list) =
   let self_tree = tree_of_lval (name, exp_list) in
+  let check_type_mismatch = check_type_mismatch self_tree in
+  let check_all_int () =
+    List.iter exp_list ~f:(fun exp ->
+        check_type_mismatch IntType (typecheck_exp ctxes exp) (tree_of_exp exp))
+  in
+  let get_type_after_indexing dims =
+    let l_dims = List.length dims in
+    let l_idxes = List.length exp_list in
+    if l_dims = l_idxes - 1 then IntType
+    else if l_dims < l_idxes - 1 then
+      fail_with_tree self_tree "the dimension of indexes is too much"
+    else ArrayType (drop_head_n dims l_idxes)
+  in
   match lookup (snd ctxes) name with
   | None -> id_not_found_error name
-  | Some ty -> (
-      match ty with
-      | IntType ->
-          if list_nonempty exp_list then
-            failwith (sp "try to treat %s:int as array" name);
-          IntType
-      | ArrayType dims ->
-          if list_empty exp_list then ArrayType dims
-          else (
-            List.iter exp_list ~f:(fun exp ->
-                check_type_mismatch IntType (typecheck_exp ctxes exp)
-                  (tree_of_exp exp) self_tree);
-            let l_dims = List.length dims in
-            let l_idxes = List.length exp_list in
-            if l_dims = l_idxes - 1 then IntType
-            else if l_dims < l_idxes - 1 then (
-              PrintBox_text.output stdout self_tree;
-              print_endline "";
-              failwith "the dimension of index is too much")
-            else ArrayType (drop_head_n dims l_idxes))
-      | _ -> failwith "the lval must be of either int or array")
+  | Some IntType ->
+      if list_nonempty exp_list then
+        fail_with_tree self_tree (sp "can not index over an int:%s" name);
+      IntType
+  | Some (ArrayType dims) ->
+      if list_empty exp_list then ArrayType dims
+      else (
+        check_all_int ();
+        get_type_after_indexing dims)
+  | _ -> failwith "the lval must be of either int or array"
 
 and typecheck_primary_exp ctxes exp =
   match exp with
@@ -40,112 +42,115 @@ and typecheck_primary_exp ctxes exp =
 
 and typecheck_unary_exp ctxes exp =
   let self_tree = tree_of_unary_exp exp in
+  let check_type_mismatch = check_type_mismatch self_tree in
+  let check_args_paras args paras =
+    zip args paras
+    |> List.iter ~f:(fun (arg, para) ->
+           check_type_mismatch para (typecheck_exp ctxes arg)
+             (tree_of_unary_exp exp))
+  in
   match exp with
   | UnaryPrimary primary_exp -> typecheck_primary_exp ctxes primary_exp
   | Call (name, args) -> (
       match lookup (fst ctxes) name with
       | None -> failwith (sp "function:%s is not defined" name)
-      | Some ty -> (
-          match ty with
-          | FuncType (return_type, paras) ->
-              zip args paras
-              |> List.iter ~f:(fun (arg, para) ->
-                     check_type_mismatch para (typecheck_exp ctxes arg)
-                       (tree_of_unary_exp exp) self_tree);
-              return_type
-          | _ -> failwith (sp "try to call on non-function: %s" name)))
+      | Some (FuncType (return_type, paras)) ->
+          check_args_paras args paras;
+          return_type
+      | _ -> failwith (sp "try to call on non-function: %s" name))
   | UnaryOp (_, unary_exp) ->
       check_type_mismatch IntType
         (typecheck_unary_exp ctxes unary_exp)
-        (tree_of_unary_exp unary_exp)
-        self_tree;
+        (tree_of_unary_exp unary_exp);
       IntType
 
 and typecheck_mul_exp ctxes exp =
   let self_tree = tree_of_mul_exp exp in
+  let check_type_mismatch = check_type_mismatch self_tree in
   match exp with
   | MulUnary unary_exp -> typecheck_unary_exp ctxes unary_exp
   | MulMul (mul_exp, _, unary_exp) ->
       check_type_mismatch IntType
         (typecheck_mul_exp ctxes mul_exp)
-        (tree_of_mul_exp mul_exp) self_tree;
+        (tree_of_mul_exp mul_exp);
       check_type_mismatch IntType
         (typecheck_unary_exp ctxes unary_exp)
-        (tree_of_unary_exp unary_exp)
-        self_tree;
+        (tree_of_unary_exp unary_exp);
       IntType
 
 and typecheck_add_exp ctxes exp =
   let self_tree = tree_of_add_exp exp in
+  let check_type_mismatch = check_type_mismatch self_tree in
   match exp with
   | AddMul mul_exp -> typecheck_mul_exp ctxes mul_exp
   | AddAdd (add_exp, mul_exp) | AddSub (add_exp, mul_exp) ->
       check_type_mismatch IntType
         (typecheck_add_exp ctxes add_exp)
-        (tree_of_add_exp add_exp) self_tree;
+        (tree_of_add_exp add_exp);
       check_type_mismatch IntType
         (typecheck_mul_exp ctxes mul_exp)
-        (tree_of_mul_exp mul_exp) self_tree;
+        (tree_of_mul_exp mul_exp);
       IntType
 
 and typecheck_rel_exp ctxes exp =
   let self_tree = tree_of_rel_exp exp in
+  let check_type_mismatch = check_type_mismatch self_tree in
   match exp with
   | RelAdd add_exp -> typecheck_add_exp ctxes add_exp
   | RelRel (rel_exp, _, add_exp) ->
       check_type_mismatch IntType
         (typecheck_add_exp ctxes add_exp)
-        (tree_of_add_exp add_exp) self_tree;
+        (tree_of_add_exp add_exp);
       check_type_mismatch IntType
         (typecheck_rel_exp ctxes rel_exp)
-        (tree_of_rel_exp rel_exp) self_tree;
+        (tree_of_rel_exp rel_exp);
       IntType
 
 and typecheck_eq_exp ctxes exp =
   let self_tree = tree_of_eq_exp exp in
+  let check_type_mismatch = check_type_mismatch self_tree in
   match exp with
   | EqRel rel_exp -> typecheck_rel_exp ctxes rel_exp
   | EqEq (eq_exp, rel_exp) | EqNeq (eq_exp, rel_exp) ->
       check_type_mismatch IntType
         (typecheck_eq_exp ctxes eq_exp)
-        (tree_of_eq_exp eq_exp) self_tree;
+        (tree_of_eq_exp eq_exp);
       check_type_mismatch IntType
         (typecheck_rel_exp ctxes rel_exp)
-        (tree_of_rel_exp rel_exp) self_tree;
+        (tree_of_rel_exp rel_exp);
       IntType
 
 and typecheck_l_and_exp ctxes exp =
   let self_tree = tree_of_l_and_exp exp in
+  let check_type_mismatch = check_type_mismatch self_tree in
   match exp with
   | AndEq andeq -> typecheck_eq_exp ctxes andeq
   | AndAnd (l_and_exp, eq_exp) ->
       check_type_mismatch IntType
         (typecheck_l_and_exp ctxes l_and_exp)
-        (tree_of_l_and_exp l_and_exp)
-        self_tree;
+        (tree_of_l_and_exp l_and_exp);
       check_type_mismatch IntType
         (typecheck_eq_exp ctxes eq_exp)
-        (tree_of_eq_exp eq_exp) self_tree;
+        (tree_of_eq_exp eq_exp);
       IntType
 
 and typecheck_exp ctxes exp =
   let self_tree = tree_of_exp exp in
+  let check_type_mismatch = check_type_mismatch self_tree in
   match exp with
   | OrAnd orand -> typecheck_l_and_exp ctxes orand
   | OrOr (l_or_exp, l_and_exp) ->
       check_type_mismatch IntType
         (typecheck_l_and_exp ctxes l_and_exp)
-        (tree_of_l_and_exp l_and_exp)
-        self_tree;
+        (tree_of_l_and_exp l_and_exp);
       check_type_mismatch IntType
         (typecheck_exp ctxes l_or_exp)
-        (tree_of_exp l_or_exp) self_tree;
+        (tree_of_exp l_or_exp);
       IntType
 
-let update_var_def ctxes var_def =
-  match var_def with
+let update_var_def ctxes = function
   | DefVar (id, exp) ->
-      check_type_mismatch IntType (typecheck_exp ctxes exp) (tree_of_exp exp)
+      check_type_mismatch (tree_of_exp exp) IntType (typecheck_exp ctxes exp)
         (tree_of_exp exp);
       update_ctxes Var ctxes id IntType
   | DefArr (id, []) -> update_ctxes Var ctxes id IntType
@@ -169,20 +174,20 @@ and check_block ctxes block expected =
 
 and check_stmt ctxes stmt expected =
   let self_tree = tree_of_stmt stmt in
+  let check_type_mismatch = check_type_mismatch self_tree in
   match stmt with
   | Assign (lval, exp) ->
       check_type_mismatch IntType
         (typecheck_lval ctxes lval)
-        (tree_of_lval lval) self_tree;
+        (tree_of_lval lval);
       check_type_mismatch IntType (typecheck_exp ctxes exp) (tree_of_stmt stmt)
-        self_tree
   | Expr exp -> ignore (typecheck_exp ctxes exp)
   | Block block ->
       ignore ((push_new_var_ctx ctxes [] |> check_block) block expected)
   | If (guard, then_, else_maybe) -> (
       check_type_mismatch IntType
         (typecheck_exp ctxes guard)
-        (tree_of_exp guard) self_tree;
+        (tree_of_exp guard);
       check_stmt ctxes then_ expected;
       match else_maybe with
       | Some else_ -> check_stmt ctxes else_ expected
@@ -190,16 +195,16 @@ and check_stmt ctxes stmt expected =
   | While (guard, stmt) ->
       check_type_mismatch IntType
         (typecheck_exp ctxes guard)
-        (tree_of_exp guard) self_tree;
+        (tree_of_exp guard);
       check_stmt ctxes stmt expected
   | Break -> ()
   | Continue -> ()
   | Return exp -> (
       match exp with
-      | None -> check_type_mismatch expected VoidType self_tree self_tree
+      | None -> check_type_mismatch expected VoidType self_tree
       | Some exp ->
           check_type_mismatch expected (typecheck_exp ctxes exp)
-            (tree_of_exp exp) self_tree)
+            (tree_of_exp exp))
 
 let update_func_def ctxes (return_type, name, func_f_params, block) =
   let new_vars =
@@ -221,8 +226,7 @@ let update_func_def ctxes (return_type, name, func_f_params, block) =
   ignore (check_block ctxes block return_type);
   ctxes
 
-let update_either_decl_funcdef ctxes either =
-  match either with
+let update_either_decl_funcdef ctxes = function
   | DeclGlobal (_, decl) -> update_decl ctxes decl
   | FuncDef func_def -> update_func_def ctxes func_def
 
