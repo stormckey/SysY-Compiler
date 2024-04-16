b421beb3f391e2b121b13dc50badb9619913e8f9
stormckey <sortygraph@gmail.com>
2024-04-16 12:59:25 +0800

    more concise lab1 2 entry

--- src/lab2.ml
+++ src/lab2.ml
@@ -1,23 +1,6 @@
 open Core
 open Runtime
-
-let make_parse_error_msg filebuf =
-  let column_offset (pos : Lexing.position) = pos.pos_cnum - pos.pos_bol in
-  let start_pos = Lexing.lexeme_start_p filebuf in
-  let start_char = column_offset start_pos in
-  let end_char = Lexing.lexeme_end_p filebuf |> column_offset in
-  Printf.sprintf "Parse error: line %d, characters %d-%d"
-    (start_pos.pos_lnum - 7) start_char end_char
-
-let parse_to_ast inputbuf =
-  try
-    let ast = Parser.source Lexer.read inputbuf in
-    Tree.ast_to_tree ast |> PrintBox_text.output stdout;
-    print_endline "";
-    ast
-  with _ ->
-    make_parse_error_msg inputbuf |> print_endline;
-    exit 1
+open Main
 
 let run filename =
   let init_ctx = ([ Map.Poly.empty ], [ Map.Poly.empty ]) in
@@ -26,12 +9,4 @@ let run filename =
   |> Typechecker.typecheck init_ctx;
   print_endline "Type Checked"
 
-let main () =
-  Command.basic ~summary:"simple compiler for SysY"
-    (let%map_open.Command filename =
-       anon (maybe_with_default "test.sy" ("filename" %: string))
-     in
-     fun () -> run filename)
-  |> Command_unix.run
-
-let () = main ()
+let () = main run
